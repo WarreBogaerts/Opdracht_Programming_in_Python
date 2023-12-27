9c0494882cc0abe31bd6bff40556e41891431893
Warre Bogaerts <warre.bogaerts@student.vives.be>
2023-12-27 19:15:46 +0100

    Adding export code lines. End of tasks.

--- UserInteraction.py
+++ UserInteraction.py
@@ -1,4 +1,6 @@
 import TaskManagerClass as TM
+import pandas as pd
+
 task_manager = TM.TaskManager()
 
 start = input("Wat wil je doen? Voor help typ h. ")
@@ -6,11 +8,13 @@ start = input("Wat wil je doen? Voor help typ h. ")
 if start.lower() == "h":
 	print(
 		'''
-		Om een taak toe te voegen reageer met a/A.
-		Om een taak te starten reageer met s/S.
-		Om een taak te voltooien reageer met c/C.
-		Om een taak te verwijderen reageer met d/D.
-		Om alle taken te zien reageer met t/T.
+		Om een taak toe te voegen, reageer met a/A.
+		Om een taak te starten, reageer met s/S.
+		Om een taak te voltooien, reageer met c/C.
+		Om een taak te verwijderen, reageer met d/D.
+		Om alle taken te zien, reageer met t/T.
+		Om een rapport naar .csv te exporteren, reageer met ec/EC.
+		Om een rapport naar excel te exporteren, reageer met ex/EX.
 		'''
 		)
 
@@ -51,6 +55,21 @@ if start.lower() == "h":
 		for row in rows:
 			print(row)
 
+	elif start.lower() == "ec":
+		#.csv exporteren.
+		data = task_manager.display_tasks("SELECT Title, Description, Name FROM Tasks JOIN Status on Tasks.Status_Id = Status.Status_Id")
+		df = pd.DataFrame(data)
+		df.to_csv("tasks.csv", sep=",", index=False)
+
+	elif start.lower() == "ex":
+		#Excel exporteren
+		data = task_manager.display_tasks("SELECT Title, Description, Name FROM Tasks JOIN Status on Tasks.Status_Id = Status.Status_Id")
+		df = pd.DataFrame(data)
+		df.to_excel("tasks.xlsx", sheet_name="tasklist", index=False)
+
+
+
+
 else:
 	if start.lower() == "a":
 		print("Geef volgende zaken door. ")
@@ -86,6 +105,18 @@ else:
 		rows = task_manager.display_tasks("SELECT Title, Description, Name FROM Tasks JOIN Status on Tasks.Status_Id = Status.Status_Id")
 		for row in rows:
 			print(row)
+
+	elif start.lower() == "ec":
+		#.csv exporteren.
+		data = task_manager.display_tasks("SELECT Title, Description, Name FROM Tasks JOIN Status on Tasks.Status_Id = Status.Status_Id")
+		df = pd.DataFrame(data)
+		df.to_csv("tasks.csv", sep=",", index=False)
+
+	elif start.lower() == "ex":
+		#Excel exporteren
+		data = task_manager.display_tasks("SELECT Title, Description, Name FROM Tasks JOIN Status on Tasks.Status_Id = Status.Status_Id")
+		df = pd.DataFrame(data)
+		df.to_excel("tasks.xlsx", sheet_name="tasklist", index=False)
 
 	else:
 		print("U geeft de verkeerde letter in. Geef a,s,c,d of t in om verder te kunnen. Voor help typ h. ")
